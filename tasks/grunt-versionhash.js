e500b948ae3e6d3d151316470f6d3e883ad6f234
Yiyuan Bai <eldereal@gmail.com>
2016-04-28 10:53:18 +0800

    support auto detect version hash in bugfix branches

--- tasks/grunt-versionhash.js
+++ tasks/grunt-versionhash.js
@@ -7,16 +7,33 @@ module.exports = function(grunt){
             grunt.log.writeln("Version is set: " + version_hash);
         }else{
             var done = this.async();
-            exec('git rev-parse HEAD', function(err, out){
-                if(err){
-                    done(false);
+            exec('git branch', function(err, out){
+                var hash;
+                var match;
+                if(!err){
+                    match = /^\s*\*\s+.*bugfix\-([0-9a-f]{7}).*$/mi.exec(out);
+                    if(match){
+                        hash = match[1];
+                    }
+                }
+                if(!hash){
+                    exec('git rev-parse HEAD', function(err, out){
+                        if(err){
+                            done(false);
+                        }else{
+                            var version_hash = out.substring(0,7);
+                            grunt.log.writeln("Version: " + version_hash);
+                            grunt.config("versionhash", version_hash);
+                            done(true);
+                        }
+                    });
                 }else{
-                    var version_hash = out.substring(0,7);
-                    grunt.log.writeln("Version: " + version_hash);
-                    grunt.config("versionhash", version_hash);
+                    grunt.log.writeln("In a bugfix branch, use original version: " + hash);
+                    grunt.config("versionhash", hash);
                     done(true);
                 }
             });
+
         }
     });
 };
