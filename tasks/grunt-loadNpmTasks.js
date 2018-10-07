4decfe99dc7be901c203c5bb68672675215355b0
Yiyuan Bai <eldereal@gmail.com>
2018-10-07 12:51:05 +0800

    会展开上层目录中的子模块

--- tasks/grunt-loadNpmTasks.js
+++ tasks/grunt-loadNpmTasks.js
@@ -3,4 +3,9 @@ var path = require("path");
 module.exports = function(grunt){
     var files = grunt.file.expand(path.resolve(__dirname, '..', 'node_modules', 'grunt-*/tasks'));
     files.forEach(grunt.loadTasks);
+    
+    var files = grunt.file.expand(path.resolve(__dirname, '..', '..', 'grunt-*/tasks'));
+    files = files.filter(f => f.indexOf('grunt-webdev') < 0);
+    files.forEach(grunt.loadTasks);
+    
 };
